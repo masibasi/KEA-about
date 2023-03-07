072895fcc9ffac8607dc45e5cebde88c0f6520c5
chromato99 <chromato99@gmail.com>
2023-03-07 21:47:04 +0900

    Add react router for navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import Navbar from './components/Navbar'
-// import logo from './logo.svg';
-// import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './components/Layout'
+import Home from './components/Home'
+import Vision from './components/Vision'
+import TeamInfo from './components/TeamInfo';
+import Contact from './components/Contact'
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="vision" element={<Vision />} />
+          <Route path="teaminfo" element={<TeamInfo />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
