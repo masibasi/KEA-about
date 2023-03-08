b88d05753ff42cc19332ea495e9995a96ad2a18d
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-03-08 10:55:50 +0900

    feat: add contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
 
 export default function Contact() {
+  const teamInfo = {
+    tel: "(031) 123-1234",
+    github: "https://github.com/KEA-ACCELER",
+    email: "KEA-ACCELER@gachon.ac.kr",
+  };
+
   return (
-    <div>Contact</div>
-  )
+    <>
+      <Container
+        className="text-center"
+        style={{ width: "50%", paddingTop: "2rem" }}
+      >
+        <Row>
+          <div
+            style={{
+              fontSize: "22px",
+              padding: "1rem",
+            }}
+          >
+            Contact Us
+          </div>
+        </Row>
+        <Row>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Email
+            </InputGroup.Text>
+            <Form.Control type="text" value={teamInfo.email} readOnly />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Github
+            </InputGroup.Text>
+            <Form.Control type="text" value={teamInfo.github} readOnly />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Tel
+            </InputGroup.Text>
+            <Form.Control type="text" value={teamInfo.tel} readOnly />
+          </InputGroup>
+        </Row>
+        <Button style={{ backgroundColor: "darkblue" }}>Contact</Button>{" "}
+      </Container>
+    </>
+  );
 }
