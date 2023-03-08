470786ede7ce3e331f160e181fdbedcd32daf32d
masibasi <jimin_lee98@naver.com>
2023-03-08 15:09:57 +0900

    [home] add description, logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
     return (
-        <div>
+        <div className="App">
             <Navbar />
             <Home />
             {/* <header className="App-header">
