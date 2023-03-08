a79c3ed91d6eea4a399eb5d3a9279a6477c5caf9
Ji Min Lee <60805546+masibasi@users.noreply.github.com>
2023-03-08 19:21:13 +0900

    Create Home (#4)
    * Create Home.js
    
    * [home] add description, logo
    
    * <chore> change font size
    
    * Delete Navbar.js
    
    * Update .gitignore
    
    ---------
    
    Co-authored-by: Beomgi Kim | 김범기 <chromato99@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './components/Layout'
-import Home from './components/Home'
-import Vision from './components/Vision'
-import TeamInfo from './components/TeamInfo';
-import Contact from './components/Contact'
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Vision from "./components/Vision";
+import TeamInfo from "./components/TeamInfo";
+import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="vision" element={<Vision />} />
-          <Route path="teaminfo" element={<TeamInfo />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="vision" element={<Vision />} />
+                    <Route path="teaminfo" element={<TeamInfo />} />
+                    <Route path="contact" element={<Contact />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import React from "react";
+import video from "../assets/space.mp4";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className="container">
+            <div className="intro_wrapper">
+                <div className="catch_phrase">
+                    <div className="catch_phrase_1">We provide</div>
+                    <div>"Advanced Coding, Cloud Engineering,</div>
+                    <div>and Lifelong Execution with Reliability"</div>
+                </div>
+                {/* <img
+                    className="main_img"
+                    src={require("../assets/acceler_bg.jpg")}
+                    alt=""
+                /> */}
+                <video
+                    className="main_vid"
+                    autoPlay={true}
+                    muted={true}
+                    loop={true}
+                    playsInline={true}
+                    src={video}
+                />
+            </div>
+            <div className="desc_wrapper">
+                <div className="description">
+                    <div>Our team Acceler, As a student team in Gachon</div>
+                    <div>
+                        Participating in KEA, is ready to do our best in the
+                        training
+                    </div>
+                    <div>
+                        and to do our best to develop our capabilities through
+                        various projects.
+                    </div>
+                </div>
+            </div>
+            <div className="logo_wrapper">
+                <img
+                    className="logo_acceler"
+                    src={require("../assets/acceler.png")}
+                    alt=""
+                />
+                <img
+                    className="logo_gachon"
+                    src={require("../assets/gachon.png")}
+                    alt=""
+                />
+                <img
+                    className="logo_kakao"
+                    src={require("../assets/Kakao_enterprise.png")}
+                    alt=""
+                />
+            </div>
+        </div>
+    );
 }
