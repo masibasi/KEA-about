a82153361e1befd36592779f2e746c7bd2246499
masibasi <jimin_lee98@naver.com>
2023-03-09 11:31:57 +0900

    feat : working on Responsive view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,62 @@
 import React from "react";
 import video from "../assets/space.mp4";
-import { useMediaQuery } from "react-responsive";
+import { Pc, Mobile } from "./Responsive";
 
 export default function Home() {
-    const Desktop = ({ children }) => {
-        const isDesktop = useMediaQuery({ minWidth: 992 });
-        return isDesktop ? children : null;
-    };
     return (
-        <Desktop>
-            <div className="container">
-                <div className="intro_wrapper">
-                    <div className="catch_phrase">
-                        <div className="catch_phrase_1">We provide</div>
-                        <div>"Advanced Coding, Cloud Engineering,</div>
-                        <div>and Lifelong Execution with Reliability"</div>
-                    </div>
-                    <video
-                        className="main_vid"
-                        autoPlay={true}
-                        muted={true}
-                        loop={true}
-                        playsInline={true}
-                        src={video}
-                    />
-                </div>
-                <div className="desc_wrapper">
-                    <div className="description">
-                        <div>Our team Acceler, As a student team in Gachon</div>
-                        <div>
-                            Participating in KEA, is ready to do our best in the
-                            training
+        <>
+            <Pc>
+                <div className="container">
+                    <div className="intro_wrapper">
+                        <div className="catch_phrase">
+                            <div className="catch_phrase_1">We provide</div>
+                            <div>"Advanced Coding, Cloud Engineering,</div>
+                            <div>and Lifelong Execution with Reliability"</div>
                         </div>
-                        <div>
-                            and to do our best to develop our capabilities
-                            through various projects.
+                        <video
+                            className="main_vid"
+                            autoPlay={true}
+                            muted={true}
+                            loop={true}
+                            playsInline={true}
+                            src={video}
+                        />
+                    </div>
+                    <div className="desc_wrapper">
+                        <div className="description">
+                            <div>
+                                Our team Acceler, As a student team in Gachon
+                            </div>
+                            <div>
+                                Participating in KEA, is ready to do our best in
+                                the training
+                            </div>
+                            <div>
+                                and to do our best to develop our capabilities
+                                through various projects.
+                            </div>
                         </div>
                     </div>
+                    <div className="logo_wrapper">
+                        <img
+                            className="logo_acceler"
+                            src={require("../assets/acceler.png")}
+                            alt=""
+                        />
+                        <img
+                            className="logo_gachon"
+                            src={require("../assets/gachon.png")}
+                            alt=""
+                        />
+                        <img
+                            className="logo_kakao"
+                            src={require("../assets/Kakao_enterprise.png")}
+                            alt=""
+                        />
+                    </div>
                 </div>
-                <div className="logo_wrapper">
-                    <img
-                        className="logo_acceler"
-                        src={require("../assets/acceler.png")}
-                        alt=""
-                    />
-                    <img
-                        className="logo_gachon"
-                        src={require("../assets/gachon.png")}
-                        alt=""
-                    />
-                    <img
-                        className="logo_kakao"
-                        src={require("../assets/Kakao_enterprise.png")}
-                        alt=""
-                    />
-                </div>
-            </div>
-        </Desktop>
+            </Pc>
+            <Mobile>hi</Mobile>
+        </>
     );
 }
