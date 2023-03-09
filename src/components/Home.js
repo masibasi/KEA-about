e2065e2935e37584eee964d66a300ef641729282
masibasi <jimin_lee98@naver.com>
2023-03-09 12:07:52 +0900

    feat : make home.js responsive to screen size

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,59 +4,102 @@ import { Pc, Mobile } from "./Responsive";
 
 export default function Home() {
     return (
-        <>
+        <div className="home">
             <Pc>
-                <div className="container">
-                    <div className="intro_wrapper">
-                        <div className="catch_phrase">
-                            <div className="catch_phrase_1">We provide</div>
-                            <div>"Advanced Coding, Cloud Engineering,</div>
-                            <div>and Lifelong Execution with Reliability"</div>
-                        </div>
-                        <video
-                            className="main_vid"
-                            autoPlay={true}
-                            muted={true}
-                            loop={true}
-                            playsInline={true}
-                            src={video}
-                        />
+                <div className="intro_wrapper">
+                    <div className="catch_phrase">
+                        <div className="catch_phrase_1">We provide</div>
+                        <div>"Advanced Coding, Cloud Engineering,</div>
+                        <div>and Lifelong Execution with Reliability"</div>
                     </div>
-                    <div className="desc_wrapper">
-                        <div className="description">
-                            <div>
-                                Our team Acceler, As a student team in Gachon
-                            </div>
-                            <div>
-                                Participating in KEA, is ready to do our best in
-                                the training
-                            </div>
-                            <div>
-                                and to do our best to develop our capabilities
-                                through various projects.
-                            </div>
+                    <video
+                        className="main_vid"
+                        autoPlay={true}
+                        muted={true}
+                        loop={true}
+                        playsInline={true}
+                        src={video}
+                    />
+                </div>
+                <div className="desc_wrapper">
+                    <div className="description">
+                        <div>Our team Acceler, As a student team in Gachon</div>
+                        <div>
+                            Participating in KEA, is ready to do our best in the
+                            training
                         </div>
-                    </div>
-                    <div className="logo_wrapper">
-                        <img
-                            className="logo_acceler"
-                            src={require("../assets/acceler.png")}
-                            alt=""
-                        />
-                        <img
-                            className="logo_gachon"
-                            src={require("../assets/gachon.png")}
-                            alt=""
-                        />
-                        <img
-                            className="logo_kakao"
-                            src={require("../assets/Kakao_enterprise.png")}
-                            alt=""
-                        />
+                        <div>
+                            and to do our best to develop our capabilities
+                            through various projects.
+                        </div>
                     </div>
                 </div>
+                <div className="logo_wrapper">
+                    <img
+                        className="logo_acceler"
+                        src={require("../assets/acceler.png")}
+                        alt=""
+                    />
+                    <img
+                        className="logo_gachon"
+                        src={require("../assets/gachon.png")}
+                        alt=""
+                    />
+                    <img
+                        className="logo_kakao"
+                        src={require("../assets/Kakao_enterprise.png")}
+                        alt=""
+                    />
+                </div>
             </Pc>
-            <Mobile>hi</Mobile>
-        </>
+            <Mobile>
+                <div className="intro_wrapper_mobile">
+                    <div className="catch_phrase">
+                        <div className="catch_phrase_1">We provide</div>
+                        <div>"Advanced Coding, Cloud Engineering,</div>
+                        <div>and Lifelong Execution with Reliability"</div>
+                    </div>
+                    <video
+                        className="main_vid_mobile"
+                        autoPlay={true}
+                        muted={true}
+                        loop={true}
+                        playsInline={true}
+                        src={video}
+                    />
+                </div>
+                <div className="desc_wrapper">
+                    <div className="description">
+                        <div>Our team Acceler</div>
+                        <div>As a student team in Gachon</div>
+                        <div>
+                            Participating in KEA, is ready to do our best in the
+                            training
+                        </div>
+                        <div>
+                            and to do our best to develop our capabilities
+                            through various projects.
+                        </div>
+                    </div>
+                </div>
+                <div className="logo_wrapper">
+                    <img
+                        className="logo_acceler"
+                        src={require("../assets/acceler.png")}
+                        alt=""
+                    />
+                    <img
+                        className="logo_gachon"
+                        src={require("../assets/gachon.png")}
+                        alt=""
+                    />
+                    <img
+                        className="logo_kakao"
+                        src={require("../assets/Kakao_enterprise.png")}
+                        alt=""
+                    />
+                </div>
+            </Mobile>
+        </div>
     );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,38 +1,38 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Outlet } from 'react-router-dom';
-import './Layout.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Outlet } from "react-router-dom";
+import "./Layout.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Layout() {
-  return (
-    <div>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="/">
-            <div id="cropped_logo_acceler">
-              <img
-                className="logo_acceler"
-                src={require("../assets/acceler.png")}
-                alt=""
-              />
-            </div>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="vision">Vision</Nav.Link>
-              <Nav.Link href="teaminfo">Team Info</Nav.Link>
-              <Nav.Link href="contact">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Outlet />
-    </div>
-  );
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <div id="cropped_logo_acceler">
+                            <img
+                                className="logo_acceler"
+                                src={require("../assets/acceler.png")}
+                                alt=""
+                            />
+                        </div>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="vision">Vision</Nav.Link>
+                            <Nav.Link href="teaminfo">Team Info</Nav.Link>
+                            <Nav.Link href="contact">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Outlet />
+        </div>
+    );
 }
 
 export default Layout;
